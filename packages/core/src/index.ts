a2b9e5c83a6eb6be7edc1df6bde47e2d62328d13
LEECHANHYUNG <lsj721538@gmail.com>
2026-03-31 23:29:53 +0900

    feat: core 패키지 생성 및 PandaCSS 설정 이관
    - packages/core 패키지 생성 (panda.config.mjs, codegen, watch 스크립트)
    - 루트의 PandaCSS 설정을 core로 이동
    - components에서 core 의존성 연결 및 스토리북 CSS import 추가
    - Button 스토리 추가

---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./Button";
+export type { ButtonProps } from "./Button";
+export * from "../styled-system/css";
+export * from "../styled-system/patterns";
+export * from "../styled-system/tokens";
